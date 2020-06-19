6ced658bc0b641725f7b32fda9b8a356e96d073a
Vitaliy Vlasov <siphiuel@gmail.com>
2020-06-19 14:10:24 +0300

    Use RN's OkHttpClient

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -18,6 +18,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.net.HttpURLConnection;
 
+import com.facebook.react.modules.network.OkHttpClientProvider;
+
+
 
 import static okhttp3.internal.Util.UTF_8;
 
@@ -177,12 +180,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     };
 
 
-    httpClient = new Builder()
-      .followRedirects(false)
-      .followSslRedirects(false)
-      .build();
-
-  }
+    httpClient = OkHttpClientProvider.getOkHttpClient();
 
   public RNCWebViewManager(WebViewConfig webViewConfig) {
     mWebViewConfig = webViewConfig;
